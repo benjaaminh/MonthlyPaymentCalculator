6e87b6e804eeaa66a6480624a40d4ac864d9cb72
benjaaminh <benjamin.heikkala@abo.fi>
2024-01-24 15:15:39 +0200

    fixed auto appending jsessionid to static resources

--- MonthlyPaymentCalculator/src/main/java/com/example/MonthlyPaymentCalculator/config/ProspectConfig.java
+++ MonthlyPaymentCalculator/src/main/java/com/example/MonthlyPaymentCalculator/config/ProspectConfig.java
@@ -1,21 +1,34 @@
 package com.example.MonthlyPaymentCalculator.config;
 
 import com.example.MonthlyPaymentCalculator.repository.ProspectRepository;
+
+import jakarta.servlet.SessionTrackingMode;
+
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
+import java.util.Set;
 
 import static com.example.MonthlyPaymentCalculator.Calculate.initialData;
 
 @Configuration
 public class ProspectConfig {
 
+    //to prevent thymeleaf from appending sessionid to static resources
     @Bean
-    CommandLineRunner commandLineRunner(ProspectRepository repository){
+    public ServletContextInitializer servletContextInitializer() {
+        return servletContext -> {
+            servletContext.setSessionTrackingModes(Set.of(SessionTrackingMode.COOKIE));
+        };
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(ProspectRepository repository) {
         return args -> {
-        repository.saveAll(List.of(initialData()));
+            repository.saveAll(List.of(initialData()));
         };
     }
 }
